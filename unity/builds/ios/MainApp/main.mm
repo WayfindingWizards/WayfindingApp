80050c315a668ff16fc846fcaec756aa2a757124
Ethan M <143635606+eMack27@users.noreply.github.com>
2024-02-23 10:12:49 -0600

    Changed Bundle Identifier, Reimported Unity iOS Build

--- unity/builds/ios/MainApp/main.mm
+++ unity/builds/ios/MainApp/main.mm
@@ -23,10 +23,6 @@ int main(int argc, char* argv[])
     @autoreleasepool
     {
         id ufw = UnityFrameworkLoad();
-        [ufw setDataBundleId: "com.unity3d.framework"];
-        // On Demand Resources are not supported in this case. To make them work instead of the calls above
-        // you need to copy Data folder to your native application (With script at Build Phases) and
-        // skip a calls above since by default Data folder expected to be in mainBundle.
         [ufw runUIApplicationMainWithArgc: argc argv: argv];
         return 0;
     }
